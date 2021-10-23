5d71230e0cfa0b8f94140ee02ef431491d8e0717
Janos Hrubos <33330538+janoshrubos@users.noreply.github.com>
2021-10-23 18:49:32 +0200

    refactor: file upload loading state

--- frontend/src/components/FileUploadForm.jsx
+++ frontend/src/components/FileUploadForm.jsx
@@ -3,58 +3,65 @@ import Message from "./Message";
 import DropZone from "./DropZone";
 import { sendEmails } from "../services/apiHandler";
 
+const FileUploadState = {
+  idle: "idle",
+  loading: "loading",
+};
+
 const FileUpload = () => {
   const [files, setFiles] = useState([]);
-  const [isLoading, setIsLoading] = useState(false);
+  const [uploadState, setUploadState] = useState(FileUploadState.idle);
   const [message, setMessage] = useState(null);
 
   const fileHandler = async (eventFiles) => {
     setMessage(null);
 
-    const emails = Array.from(eventFiles).map((file) => {
+    const data = Array.from(eventFiles).map((file) => {
       const reader = new FileReader();
       const fileExtension = file.name.split(".").pop();
 
       if (fileExtension !== "txt") {
         setMessage({
-          message: `Unsupported file extension: ${fileExtension}`,
+          message: `Unsupported file extension: ${fileExtension}. Use .txt files only!`,
           ok: false,
         });
         return Promise.reject();
       }
       return new Promise((resolve) => {
-        reader.onload = () =>
+        reader.onload = () => {
           resolve({ name: file.name, data: reader.result.trim().split("\n") });
+        };
         reader.readAsText(file);
       });
     });
 
-    const result = await Promise.all(emails);
+    const result = await Promise.all(data);
     setFiles(result);
   };
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    const emails = files.reduce((a, { data }) => [...a, ...data], [])
+    const emails = files.reduce((acc, { data }) => acc.concat(data), []);
 
-    setIsLoading(true);
     setMessage(null);
+    setUploadState(FileUploadState.loading);
 
     const { message, ok, data } = await sendEmails(emails);
 
     if (ok) {
       setFiles([]);
     }
+
     setMessage({ message, ok, data });
-    setIsLoading(false);
+    setUploadState(FileUploadState.idle);
   };
 
   return (
     <div className="container">
       <form onSubmit={handleSubmit}>
         <DropZone
-          isLoading={isLoading}
+          isLoading={uploadState === FileUploadState.loading}
           files={files}
           fileHandler={fileHandler}
         />
